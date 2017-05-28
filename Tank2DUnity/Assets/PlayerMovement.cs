06e67912f1a66aa89c83c37e11db904e10ae329f
Rafał Kampa <rafalkampa@admins-MacBook-Pro.local>
2017-05-28 22:08:36 +0200

    base proj

---
+++ Tank2DUnity/Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	private float turnSpeed = 5f;
+	private Vector2 movement;
+
+	public float moveSpeed = 10f;
+	// Use this for initialization
+	void Start () {
+		Input.simulateMouseWithTouches = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 currentPosition = transform.position;
+
+		if (Input.touchCount > 0) {
+			Debug.Log("sdsa");
+			Touch touch = Input.GetTouch(0);
+			if (touch.phase == TouchPhase.Began) {
+				Vector2 moveTowards = Camera.main.ScreenToWorldPoint(touch.position);
+				movement = moveTowards - currentPosition;
+				movement.Normalize();
+			}
+		}
+		float targetAngle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, -targetAngle), turnSpeed * Time.deltaTime);
+	}
+
+	void touch () {
+		
+	}
+}
+
